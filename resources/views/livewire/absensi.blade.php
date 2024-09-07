d3db15c2b392895dee6d1175f06b5199dc5f5e5a
chegg-son <chegg.6964@gmail.com>
2024-09-07 11:27:36 +0700

    minor fix

--- resources/views/livewire/absensi.blade.php
+++ resources/views/livewire/absensi.blade.php
@@ -4,6 +4,12 @@
             background-color: #f8fafc;
         }
 
+        .input-group {
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        }
+
         .card {
             background-color: #fff;
             border-radius: 5px;
